2fe1da21bd59dcecf2242eb5244681e8fb72bfea
Afshin Rudgar <afshinrodgar@gmail.com>
2017-05-03 11:33:31 +0430

    Change in exported module, bower is removed now all work in being done using npm

--- ga.js
+++ ga.js
@@ -1,71 +1,93 @@
-(function (angular) {
-'use strict';
+(function (root, factory) {
+  'use strict';
 
-angular.module('ga', [])
+  /* istanbul ignore next */
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    // to support bundler like browserify
+    var angularObj = angular || require('angular');
+    if ((!angularObj || !angularObj.module) && typeof angular != 'undefined') {
+      angularObj = angular;
+    }
+    module.exports = factory(angularObj);
+  } else {
+    // Browser globals (root is window)
+    factory(root.angular);
+  }
+})(this, function (angular) {
+  'use strict';
+
+  return angular.module('ga', [])
     .factory('ga', ['$window', function ($window) {
 
-        var ga = function() {
-            if (angular.isArray(arguments[0])) {
-                for(var i = 0; i < arguments.length; ++i) {
-                    ga.apply(this, arguments[i]);
-                }
-                return;
-            }
-            // console.log('ga', arguments);
-            if ($window.ga) {
-                $window.ga.apply(this, arguments);
-            }
-        };
+      var ga = function () {
+        if (angular.isArray(arguments[0])) {
+          for (var i = 0; i < arguments.length; ++i) {
+            ga.apply(this, arguments[i]);
+          }
+          return;
+        }
+        // console.log('ga', arguments);
+        if ($window.ga) {
+          $window.ga.apply(this, arguments);
+        }
+      };
 
-        return ga;
+      return ga;
     }])
     .run(['$rootScope', '$location', 'ga', function ($rootScope, $location, ga) {
-       
-        $rootScope.$on('$routeChangeStart', function() {
-            ga('set', 'page', $location.url());
-        });
+
+      $rootScope.$on('$routeChangeStart', function () {
+        ga('set', 'page', $location.url());
+      });
 
     }])
     /**
-      ga="'send', 'event', 'test'" ga-on="click|hover|init"
-      */
-    .directive('ga', ['ga', function(ga) {
-        return {
-          restrict: 'A',
-          scope: false,
-          link: function($scope, $element, $attrs) {
-            var bindToEvent = $attrs.gaOn || 'click';
+     ga="'send', 'event', 'test'" ga-on="click|hover|init"
+     */
+    .directive('ga', ['ga', function (ga) {
+      return {
+        restrict: 'A',
+        scope: false,
+        link: function ($scope, $element, $attrs) {
+          var bindToEvent = $attrs.gaOn || 'click';
 
-            var onEvent = function() {
-                var command = $attrs.ga;
-                if (command) {
-                    if (command[0] === '\'') command = '[' + command + ']';
+          var onEvent = function () {
+            var command = $attrs.ga;
+            if (command) {
+              if (command[0] === '\'') command = '[' + command + ']';
 
-                    command = $scope.$eval(command);
-                } else {
-                    // auto command
-                    var href = $element.attr('href');
-                    if (href && href === '#') href = '';
-                    var category = $attrs.gaCategory ? $scope.$eval($attrs.gaCategory) : 
-                            (href && href[0] !== '#' ? (href.match(/\/\//) ? 'link-out' : 'link-in') : 'button'),
-                        action = $attrs.gaAction ? $scope.$eval($attrs.gaAction) : 
-                            (href ? href : 'click'),
-                        label = $attrs.gaLabel ? $scope.$eval($attrs.gaLabel) : 
-                            ($element[0].title || ($element[0].tagName.match(/input/i) ? $element.attr('value') : $element.text())).substr(0, 64),
-                        value = $attrs.gaValue ? $scope.$eval($attrs.gaValue) : null;
-                    command = ['send', 'event', category, action, label];
-                    if (value !== null) command.push(value);
-                }
-                ga.apply(null, command);
-            };
-
-            if (bindToEvent === 'init') {
-                onEvent();
+              command = $scope.$eval(command);
             } else {
-                $element.bind(bindToEvent, onEvent);
+              // auto command
+              var href = $element.attr('href');
+              if (href && href === '#') href = '';
+              var category = $attrs.gaCategory ? $scope.$eval($attrs.gaCategory) :
+                  (href && href[0] !== '#' ? (href.match(/\/\//) ? 'link-out' : 'link-in') : 'button'),
+                action = $attrs.gaAction ? $scope.$eval($attrs.gaAction) :
+                  (href ? href : 'click'),
+                label = $attrs.gaLabel ? $scope.$eval($attrs.gaLabel) :
+                  ($element[0].title || ($element[0].tagName.match(/input/i) ? $element.attr('value') : $element.text())).substr(0, 64),
+                value = $attrs.gaValue ? $scope.$eval($attrs.gaValue) : null;
+              command = ['send', 'event', category, action, label];
+              if (value !== null) command.push(value);
             }
+            ga.apply(null, command);
+          };
+
+          if (bindToEvent === 'init') {
+            onEvent();
+          } else {
+            $element.bind(bindToEvent, onEvent);
           }
-        };
-      }]);
-})(angular);
+        }
+      };
+    }])
+    .name;
+});
 
--- test/spec.js
+++ test/spec.js
@@ -1,127 +1,126 @@
 describe('ga', function () {
-    'use strict';
-    
-    // load the service's module
-    beforeEach(module('ga'));
+  'use strict';
 
-    // instantiate service
-    var ga, window, compileElement;
-    beforeEach(inject(function (_ga_, $window, $rootScope, $compile) {
-        ga = _ga_;
-        window = $window;
-        window.gaCalled = false;
-        window.ga = function() {window.gaCalled = arguments;}
-        spyOn(window, 'ga').and.callThrough();
+  // load the service's module
+  beforeEach(module('ga'));
 
-        compileElement = function(html) {
-            return $compile(angular.element(html))($rootScope.$new());
-        }
+  // instantiate service
+  var ga, window, compileElement;
+  beforeEach(inject(function (_ga_, $window, $rootScope, $compile) {
+    ga = _ga_;
+    window = $window;
+    window.gaCalled = false;
+    window.ga = function () {
+      window.gaCalled = arguments;
+    };
+    spyOn(window, 'ga').and.callThrough();
 
-    }));
+    compileElement = function (html) {
+      return $compile(angular.element(html))($rootScope.$new());
+    };
+
+  }));
 
 
-    describe('service', function () {
+  describe('service', function () {
 
-        it('should pass params', function () {
-            ga('send', 'pageview');
-            expect(window.ga).toHaveBeenCalledWith('send', 'pageview')
-        });
-
-        it('should pass multiple params', function () {
-            ga([['set', 'page', '/'], ['send', 'pageview']]);
-            expect(window.ga.calls.first().args).toEqual(['set', 'page', '/'])
-            expect(window.ga).toHaveBeenCalledWith('send', 'pageview')
-        });
-
-        it('should not throw anything', function () {
-            delete window.ga;
-            expect(window.ga).toBeUndefined();
-            ga('send', 'pageview');
-            expect(window.gaCalled).toBeFalsy();
-        });
-
+    it('should pass params', function () {
+      ga('send', 'pageview');
+      expect(window.ga).toHaveBeenCalledWith('send', 'pageview')
     });
 
-    describe('directive', function () {
-        var el;
-        it('should call on init', function() {
-            el = compileElement('<div ga="[\'send\', \'event\', \'init\']" ga-on="init"></div>')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'init');
-        })
-
-        it('should handle shorthand array', function() {
-            el = compileElement('<div ga="\'send\', \'event\', \'short\'" ga-on="init"></div>')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'short');
-        })
-
-        it('should call on click', function() {
-            el = compileElement('<div ga="\'send\', \'event\', \'click\'"></div>')
-            expect(window.ga).not.toHaveBeenCalled();
-            el.triggerHandler('click');
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click');
-        })
-
-        it('should call on blur', function() {
-            el = compileElement('<div ga="\'send\', \'event\', \'blur\'" ga-on="blur"></div>')
-            expect(window.ga).not.toHaveBeenCalled();
-            el.triggerHandler('blur');
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'blur');
-        })
-
-        it('should handle repeated events', function() {
-            el = compileElement('<div ga="\'send\', \'event\', testValue"></div>')
-            el.scope().testValue = 'click1';
-            el.triggerHandler('click');
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click1');
-            el.scope().testValue = 'click2';
-            el.triggerHandler('click');
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click2');
-        })
-
-        it('should click button', function() {
-            el = compileElement('<div ga>Label</div>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Label');
-        })
-
-        it('should click # link', function() {
-            el = compileElement('<a href="#" ga>Label</a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Label');
-        })
-
-        it('should click anchor link', function() {
-            el = compileElement('<a href="#anchor" ga>Label</a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', '#anchor', 'Label');
-        })
-
-        it('should have title', function() {
-            el = compileElement('<a href="#anchor" title="Title" ga>Label</a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', '#anchor', 'Title');
-        })
-
-        it('should click link', function() {
-            el = compileElement('<a href="/" ga>Label</a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'link-in', '/', 'Label');
-        })
-
-        it('should click out link', function() {
-            el = compileElement('<a href="http://www.stamina.pl/" ga><b>Label</b></a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'link-out', 'http://www.stamina.pl/', 'Label');
-        })
-
-        it('should click button', function() {
-            el = compileElement('<input type="submit" value="Submit" ga />').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Submit');
-        })
-
-        it('should have special attributes', function() {
-            el = compileElement('<a href="#" title="Title" ga ga-category="\'cat\'" ga-action="\'act\'" ga-label="\'lab\'" ga-value="1">Label</a>').triggerHandler('click')
-            expect(window.ga).toHaveBeenCalledWith('send', 'event', 'cat', 'act', 'lab', 1);
-        })
-
-
-
-
+    it('should pass multiple params', function () {
+      ga([['set', 'page', '/'], ['send', 'pageview']]);
+      expect(window.ga.calls.first().args).toEqual(['set', 'page', '/'])
+      expect(window.ga).toHaveBeenCalledWith('send', 'pageview')
     });
 
+    it('should not throw anything', function () {
+      delete window.ga;
+      expect(window.ga).toBeUndefined();
+      ga('send', 'pageview');
+      expect(window.gaCalled).toBeFalsy();
+    });
+
+  });
+
+  describe('directive', function () {
+    var el;
+    it('should call on init', function () {
+      el = compileElement('<div ga="[\'send\', \'event\', \'init\']" ga-on="init"></div>')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'init');
+    });
+
+    it('should handle shorthand array', function () {
+      el = compileElement('<div ga="\'send\', \'event\', \'short\'" ga-on="init"></div>')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'short');
+    });
+
+    it('should call on click', function () {
+      el = compileElement('<div ga="\'send\', \'event\', \'click\'"></div>')
+      expect(window.ga).not.toHaveBeenCalled();
+      el.triggerHandler('click');
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click');
+    });
+
+    it('should call on blur', function () {
+      el = compileElement('<div ga="\'send\', \'event\', \'blur\'" ga-on="blur"></div>')
+      expect(window.ga).not.toHaveBeenCalled();
+      el.triggerHandler('blur');
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'blur');
+    });
+
+    it('should handle repeated events', function () {
+      el = compileElement('<div ga="\'send\', \'event\', testValue"></div>')
+      el.scope().testValue = 'click1';
+      el.triggerHandler('click');
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click1');
+      el.scope().testValue = 'click2';
+      el.triggerHandler('click');
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'click2');
+    });
+
+    it('should click button', function () {
+      el = compileElement('<div ga>Label</div>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Label');
+    });
+
+    it('should click # link', function () {
+      el = compileElement('<a href="#" ga>Label</a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Label');
+    });
+
+    it('should click anchor link', function () {
+      el = compileElement('<a href="#anchor" ga>Label</a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', '#anchor', 'Label');
+    });
+
+    it('should have title', function () {
+      el = compileElement('<a href="#anchor" title="Title" ga>Label</a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', '#anchor', 'Title');
+    });
+
+    it('should click link', function () {
+      el = compileElement('<a href="/" ga>Label</a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'link-in', '/', 'Label');
+    });
+
+    it('should click out link', function () {
+      el = compileElement('<a href="http://www.stamina.pl/" ga><b>Label</b></a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'link-out', 'http://www.stamina.pl/', 'Label');
+    });
+
+    it('should click button', function () {
+      el = compileElement('<input type="submit" value="Submit" ga />').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'button', 'click', 'Submit');
+    });
+
+    it('should have special attributes', function () {
+      el = compileElement('<a href="#" title="Title" ga ga-category="\'cat\'" ga-action="\'act\'" ga-label="\'lab\'" ga-value="1">Label</a>').triggerHandler('click')
+      expect(window.ga).toHaveBeenCalledWith('send', 'event', 'cat', 'act', 'lab', 1);
+    });
+
+
+  });
 
 });
